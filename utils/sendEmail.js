547cc572b8eadd273ef46406c0c0666f632e0631
diva-sl <divakaran.sivasamy@gmail.com>
2026-06-19 14:11:11 +0530

    update the transaction verify payment

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,30 +1,31 @@
 import nodemailer from "nodemailer";
 
-// const transporter = nodemailer.createTransport({
-//   host: process.env.SMTP_HOST,
-//   port: Number(process.env.SMTP_PORT),
-//   secure: true,
-//   auth: {
-//     user: process.env.SMTP_EMAIL,
-//     pass: process.env.SMTP_PASSWORD,
-//   },
-// });
-
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: 465,
+  port: Number(process.env.SMTP_PORT) || 465,
   secure: true,
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_PASSWORD,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   tls: {
     rejectUnauthorized: false,
   },
 });
 
+transporter.verify((error) => {
+  if (error) {
+    console.error("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+});
+
 export const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
+  return transporter.sendMail({
     from: `"AG & ASSOCIATES" <${process.env.SMTP_EMAIL}>`,
     to,
     subject,
